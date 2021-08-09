46176f460f3e29ce7be27e37a12edf29935d620d
Gabriela Costa <davidbia6@gmail.com>
2021-08-09 00:54:21 -0300

    teste do funcionamento da api

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.post('/model')
 ## Coloque seu codigo na função abaixo
-def titanic(Sex:int):
+def titanic(Sex:int, Age:float, Lifeboat:int, Pclass:int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
 
