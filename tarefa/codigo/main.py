5b8c8573eb101c3c04961e491a21d99977048167
Gabriela Costa <davidbia6@gmail.com>
2021-08-09 22:57:16 -0300

    end

--- tarefa/codigo/main.py
+++ tarefa/codigo/main.py
@@ -1,5 +1,7 @@
 import pickle
+import pandas as pd
 from fastapi import FastAPI
+from sklearn import *
 
 app = FastAPI()
 @app.post('/model')
@@ -8,6 +10,28 @@ def titanic(Sex:int, Age:float, Lifeboat:int, Pclass:int):
     with open('model/Titanic.pkl', 'rb') as fid: 
         titanic = pickle.load(fid)
 
+    prediction = titanic.predict([[Sex, Age, Lifeboat, Pclass]])
+
+    if survived==True:
+        return{
+            'survived': prediction,
+            'status': 200,
+            'message': 'Sobrevivente do naufrágio do Titanic - Predição concluída!'
+        }
+                
+    if survived==False:
+        return{
+            'survived': prediction,
+            'status': 200,
+            'message': 'Vítima do naufrágio do Titanic - Predição concluída!'
+        }
+    else:
+        return{
+            'message':'Falha na execução :|'
+        }
+
 @app.get('/model')
 def get():
-    return {'hello':'test'}
+    return {
+        "Hello World"
+    }
